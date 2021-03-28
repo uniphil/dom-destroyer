cb35e472c64d8d6e30d60263fcb4e775f87fc5cb
phil <uniphil@gmail.com>
2021-03-28 18:12:34 -0400

    swap out the whole element positioning scheme
    and destroy the last locked target, not the click target
    
    i don't remember why this didn't use `getClientBoundingRect` in the first
    place, since it seems to work pretty well? is this old enough that that api
    was still too new to rely on? :p
    
    also adds a bit of transition flair that will probably get annoying.
    
    targetting of fixed elements now works properly when the document is scrolled.

--- destroyer.js
+++ destroyer.js
@@ -8,7 +8,7 @@
   var mask = document.createElement('div');
   mask.style.boxSizing = 'border-box';
   mask.style.pointerEvents = 'none';
-  mask.style.position = 'absolute';
+  mask.style.position = 'fixed';
   mask.style.top = '0';
   mask.style.left = '0';
   mask.style.height = '0';
@@ -16,51 +16,58 @@
   mask.style.border = '2px dashed hsla(350, 50%, 50%, 0.5)'
   mask.style.background = 'radial-gradient(circle, hsla(350, 50%, 80%, 0) 25%, hsla(350, 50%, 50%, 0.25))';
   mask.style.zIndex = '9999999';
+  mask.style.transition = 'top 0.1s, left 0.1s, width 0.1s, height 0.1s';
 
-  var stack_bottom = function stack_bottom(el) {
-    // walk up the DOM while parent nodes are exactly the same size & place
-    while (el.parentNode) {
-      if (el.clientLeft   !== 0 ||
-          el.clientTop    !== 0 ||
-          el.offsetLeft   !== 0 ||
-          el.offsetTop    !== 0 ||
-          el.offsetWidth  !== el.parentNode.offsetWidth ||
-          el.offsetHeight !== el.parentNode.offsetHeight
-      ) break;
-      el = el.parentNode;
-    }
-    return el;
+  var current_target;
+
+  var lock_on = function lock_on(el, rect) {
+    current_target = el;
+    rect = rect || el.getBoundingClientRect();
+    mask.style.width = rect.width + 'px';
+    mask.style.left =  rect.left + 'px';
+    mask.style.height = rect.height + 'px';
+    mask.style.top = rect.top + 'px';
+    mask.style.display = 'block';
   }
 
-  var page_offset = function page_offset(el) {
-    var left = -el.clientLeft;
-    var top = -el.clientTop;
-    while(el) {
-      left += el.offsetLeft + el.clientLeft;
-      top += el.offsetTop + el.clientTop;
-      el = el.offsetParent;
-    }
-    return {left: left, top: top};
-  };
+  var _rafref;
+  var follow = function() {
+    cancelAnimationFrame(_rafref);
+    _rafref = requestAnimationFrame(() => {
+      if (current_target) lock_on(current_target);
+    })
+  }
+
+  var lock_off = function lock_off() {
+    current_target = null;
+    mask.style.display = 'none';
+  }
 
   var change_target = function change_target(event) {
-    var el = stack_bottom(event.target);
-    var offset = page_offset(el);
-    mask.style.width = el.offsetWidth + 'px';
-    mask.style.left = offset.left + 'px';
-    mask.style.height = el.offsetHeight + 'px';
-    mask.style.top = offset.top + 'px';
+    var el = event.target;
+    var rect = el.getBoundingClientRect();
+    while (el.parentNode && el.parentNode.getBoundingClientRect) {
+      var parentRect = el.parentNode.getBoundingClientRect();
+      if (rect.top    !== parentRect.top    ||
+          rect.right  !== parentRect.right  ||
+          rect.bottom !== parentRect.bottom ||
+          rect.left   !== parentRect.left
+      ) break;
+      el = el.parentNode;
+      rect = parentRect;
+    }
+    lock_on(el, rect);
   };
+
   var destroy = function destroy(event) {
     event.stopImmediatePropagation();
     event.stopPropagation();
     event.preventDefault();
-    var el = stack_bottom(event.target);
-    if (!el) return;
-    el.parentNode.removeChild(el);
-    mask.style.height = '0';
-    mask.style.width = '0';
+    if (!current_target) return;
+    current_target.parentNode.removeChild(current_target);
+    lock_off();
   };
+
   var maybe_disarm = function keydown(event) {
     if (event.keyCode === 27) {  // ESC
       disarm();
@@ -72,6 +79,7 @@
     document.removeEventListener('mouseover', change_target);
     document.removeEventListener('click', destroy, true);
     document.removeEventListener('keydown', maybe_disarm);
+    document.addEventListener('scroll', follow);
     window.removeEventListener('blur', disarm);
 
     chrome.runtime.sendMessage({ armed: false });
@@ -84,6 +92,7 @@
     document.addEventListener('mouseover', change_target);
     document.addEventListener('click', destroy, true);
     document.addEventListener('keydown', maybe_disarm);
+    document.addEventListener('scroll', follow);
     window.addEventListener('blur', disarm);
 
     chrome.runtime.sendMessage({ armed: true });
